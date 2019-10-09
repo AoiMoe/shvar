51573c1d1bbd1cd36a71ebccb9028cc9a3bcdfa5
Takuya SHIOZAKI <AoiMoe@imou.to>
2019-10-09 23:15:20 +0900

    add shvar__pool:foldmap/3 and map/2

--- src/shvar__pool.erl
+++ src/shvar__pool.erl
@@ -8,7 +8,9 @@
          put/3,
          keys/1,
          values/1,
-         to_list/1
+         to_list/1,
+         foldmap/3,
+         map/2
         ]).
 -export_type([
               pool/0
@@ -51,3 +53,31 @@ values(Pool) ->
 -spec to_list(pool()) -> [{key(), val()}].
 to_list(Pool) ->
     maps:to_list(Pool).
+
+%% @doc foldmap operation over Pool.
+-spec foldmap(fun((key(), val(), _Acc1) -> {_Acc2, val() | undefined}), _Acc0, pool()) -> {_Acc3, pool()}.
+foldmap(FoldMapFun, Acc0, Pool) ->
+    {Acc3, Pool3} =
+        maps:fold(
+            fun(Key, Val0, {Acc1, Pool1}) ->
+                    case FoldMapFun(Key, Val0, Acc1) of
+                        {Acc2, undefined} ->
+                            {Acc2, Pool1};
+                        {Acc2, Val1} ->
+                            {Acc2, [{Key, Val1} | Pool1]}
+                    end
+            end, {Acc0, []}, Pool),
+    {Acc3, maps:from_list(Pool3)}.
+
+%% @doc map operation over Pool.
+-spec map(fun((key(), val()) -> val() | undefined), pool()) -> pool().
+map(MapFun, Pool) ->
+    Pool2 =
+        maps:fold(
+            fun(Key, Val0, Pool1) ->
+                    case MapFun(Key, Val0) of
+                        undefined -> Pool1;
+                        Val1 -> [{Key, Val1} | Pool1]
+                    end
+            end, [], Pool),
+    maps:from_list(Pool2).
